360b43ce50b8087ec006a6c84ccda5d579494c17
krsanford <ksanford@ostusa.com>
2015-11-05 10:49:51 -0500

    initial pass at res streaming

--- app.js
+++ app.js
@@ -4,11 +4,20 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
-var users = require('./routes/users');
+
+var http = require('http');
+var port = process.env.PORT || 3001;
 
 var app = express();
+app.set('port', port);
+var server = http.createServer(app).listen(port);
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -23,7 +32,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
+var http = require('http');
 var router = express.Router();
+var $ = require('cheerio');
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+  function gotHTML(err, resp, html) {
+    if (err) return console.error(err);
+    var parsedHTML = $.load(html);
+    // get all img tags and loop over them
+    var imageURLs = [];
+    parsedHTML('img').map(function(i, link) {
+      var href = $(link).attr('src');
+      console.log(href);
+      if (!href.match('.gif')) return;
+      imageURLs.push(href);
+    });
+    request(imageURLs[0]).pipe(res);
+  };
+
+  var domain = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=krieger+archer&fmt=html';
+  request(domain, gotHTML);
 });
 
 module.exports = router;
