d9f3648f8556261bca63a30b240ccbaf95d1fe81
krsanford <ksanford@ostusa.com>
2015-11-05 11:44:45 -0500

    slimming

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var routes = require('./routes/index');
-
+var router = express.Router();
+var $ = require('cheerio');
+var request = require('request');
 var http = require('http');
 var port = process.env.PORT || 3001;
 
@@ -19,50 +15,33 @@ app.use(function(req, res, next) {
     next();
 });
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+/* GET home page. */
+router.get('/', function(req, res, next) {
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  if (Object.keys(req.query).length == 0) {
+    res.redirect('/?' + Math.floor(Math.random() * 10000000));
+    return;
+  }
+  console.log(req.query);
 
-app.use('/', routes);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
+  function gotHTML(err, resp, html) {
+    if (err) return console.error(err);
+    var parsedHTML = $.load(html);
+    // get all img tags and loop over them
+    var imageURLs = [];
+    parsedHTML('img').map(function(i, link) {
+      var href = $(link).attr('src');
+      console.log(href);
+      if (!href.match('.gif')) return;
+      imageURLs.push(href);
     });
-  });
-}
+    request(imageURLs[0]).pipe(res);
+  };
 
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+  var domain = 'http://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=krieger+archer&fmt=html';
+  request(domain, gotHTML);
 });
 
+app.use('/', router);
 
 module.exports = app;
